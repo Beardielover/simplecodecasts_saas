3c094a444f66aa3836d14067f35efce8b4a79099
Kai Ritchie <ritchiekai@gmail.com>
2015-09-03 20:27:10 +0000

    Fixed stuff

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,10 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'ritchiekai@gmail.com'
-    
-    def contact_email(name, email, body)
-        @name = name
-        @email = email
-        @body = body
-        
-        mail(from: email, subject: 'Contact Form Message')
-    end
+  default to: 'your@email_address.com'
+ 
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
